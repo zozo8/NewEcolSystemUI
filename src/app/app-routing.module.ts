50d1b119a419da18fe23f257dc8be0bf4219736f
Marek Zmarzły <marekzmarzly@gmail.com>
2022-07-01 10:13:50 +0200

    before update angular cli

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { DashboardPageComponent } from "./components/pages/dashboard-page/dashboard-page.component";
+import { MainpageComponent } from "./components/pages/dashboard/mainpage/mainpage.component";
+import { LoginComponent } from "./components/pages/login/login.component";
+import { NotfoundComponent } from "./components/pages/notfound/notfound.component";
+import { AuthGuard } from "./shared/auth.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", redirectTo:"login", pathMatch:"full"},
+  {path:"login", component:LoginComponent,canActivate:[AuthGuard]},
+  {path:"dashboard", component:DashboardPageComponent,canActivate:[AuthGuard], children:[
+    {path:"mainpage", component:MainpageComponent, canActivate:[AuthGuard]}
+  ]},
+  {path:"**", component:NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { ButtonModule } from "primeng/button";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./components/pages/login/login.component";
+import { DashboardPageComponent } from "./components/pages/dashboard-page/dashboard-page.component";
+import { MainpageComponent } from "./components/pages/dashboard/mainpage/mainpage.component";
+import { NotfoundComponent } from "./components/pages/notfound/notfound.component";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { AuthconfigInterceptor } from "./shared/authconfig.interceptor";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardPageComponent,
+    MainpageComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide:HTTP_INTERCEPTORS,
+      useClass:AuthconfigInterceptor,
+      multi:true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
