8794b9f8251996434862009d6be3f80566db6f6d
Marek Zmarzły <marekzmarzly@gmail.com>
2022-08-19 12:16:37 +0200

    change environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  endpointLoginUR: "http://nes.ecol.com.pl:7799",
+  endpointApiPath: "http://nes.ecol.com.pl:8010",
+  appVersion: "1.0.0"
 };
