dfff76471658daa1d7d44770f818a5a8c70cabac
Marek Zmarzły <marekzmarzly@gmail.com>
2022-10-24 08:31:42 +0200

    seach

--- src/app/components/pages/dashboard/tabs/tabs.component.ts
+++ src/app/components/pages/dashboard/tabs/tabs.component.ts
@@ -1,9 +1,7 @@
-import { AfterContentInit, Component, ComponentFactoryResolver, ContentChildren, EventEmitter, OnInit, Output, QueryList, ViewChild } from "@angular/core";
+import { Component, EventEmitter, Output, ViewChild } from "@angular/core";
 import { TranslateService } from "@ngx-translate/core";
-import { MenuItem} from "primeng/api";
 import { DynamicTabDirective } from "src/app/directivies/dynamic-tab.directive";
 import { Tab } from "src/app/models/tab.model";
-import { UsersComponent } from "src/app/modules/admin/pages/users/users.component";
 import { LeftMenuService } from "../../dashboard-page/left-menu/left-menu.service";
 import { MainpageComponent } from "../mainpage/mainpage.component";
 
@@ -16,7 +14,6 @@ export class TabsComponent {
 
   tabs:Tab[] = [];
   activeTab:number;
-  //parents:MenuItem[] = [];
 
   @ViewChild(DynamicTabDirective, {static:true}) dynamicTab!:DynamicTabDirective;
 
@@ -40,22 +37,13 @@ export class TabsComponent {
   }
 
   public refreshTabs(tab:Tab):void {
-    //console.log("start tab", tab);
     if (tab.component) {
       var extTab = this.tabs.findIndex(x=>x.component === tab.component);
       if(extTab === -1){
         this.tabs.push(tab);
-       // console.log("po dodaniu do tablicy", this.tabs);
-
-        //this.populateTabByComponent(tab.component);
-        //this.setParents(tab);
         this.activeTab = this.activeTab === undefined?0:(this.activeTab+1);
-        console.log("active tab - new", this.activeTab);
-
       } else {
         this.activeTab = extTab;
-        console.log("active tab - reopen", this.activeTab);
-        //this.populateTabByComponent(tab.component);
       }
     }
 
@@ -82,31 +70,8 @@ export class TabsComponent {
   //   });
   // }
 
-  changeTab(ev:any):void{
-    console.log("wybrabny tab ",ev.index);
-   // const comp = this.tabs[ev.index].component;
-   // this.populateTabByComponent(comp);
-  }
 
   closeTab(ev:any):void{
     this.tabs.splice(ev.index,1);
-    //wyliczienie ostatniegi i otwarcie go
   }
-
-  // private populateTabByComponent(component:any):void {
-  //   const viewContainerRef = this.dynamicTab.viewContainerRef;
-  //   viewContainerRef.clear();
-  //   viewContainerRef.createComponent(component);
-  // }
-
-  // addTab() {
-
-  //   this.refreshTabs({
-  //     header:"users",
-  //     component:UsersComponent,
-  //     tooltip:"jakeis tam dane dot users component",
-  //     icon:"pi pi-user"
-  //   });
-  // }
-
 }
